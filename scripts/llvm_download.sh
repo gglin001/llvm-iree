92f9aaa4fcce745a1e28f6d8e174979ed3347742
Allen Guo <guosonglin001@gmail.com>
2026-03-14 08:48:13 +0000

    deps

--- scripts/llvm_download.sh
+++ scripts/llvm_download.sh
@@ -10,7 +10,7 @@ git remote add origin git@github.com:llvm/llvm-project.git
 ######
 
 git sparse-checkout set --no-cone \
-  /cmake /llvm /mlir /lld /clang \
+  /cmake /llvm /mlir /lld /clang /libunwind \
   '!/llvm/test' '!/llvm/unittests' '!/llvm/docs' \
   '!/mlir/test' '!/mlir/unittests' '!/mlir/docs' \
   '!/clang/test' '!/clang/unittests' '!/clang/docs' \
